020ad652cc393ef32094d7bac3072f42eef589cd
Crystal Qian <crystal_qian@cable.comcast.net>
2015-06-29 10:14:58 -0600

    Added error checking, also elementary file writer

--- sqlToJson/sqlToJson.go
+++ sqlToJson/sqlToJson.go
@@ -10,18 +10,20 @@ import (
 	"log"
 )
 
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
 //returns *Rows from queried database
 func GetRows(username string, password string, environment string, tableName string) *sql.Rows {
 	//opens database
 	db, err := sql.Open("mysql", username+":"+password+"@tcp(localhost:3306)/"+environment)
-	if err != nil {
-		panic(err.Error())
-	}
+	check(err)
 
 	rows, err := db.Query("SELECT * from asn")
-	if err != nil {
-		log.Fatal(err)
-	}
+	check(err)
 
 	return rows
 }
@@ -36,9 +38,8 @@ func MakeJsonByteArray(rows *sql.Rows) [][]byte {
 		var c int
 		var d string
 		err := rows.Scan(&a, &b, &c, &d)
-		if err != nil {
-			log.Fatal(err)
-		}
+		check(err)
+
 		tableInstance := structs.AsnStruct{a, b, c, d}
 		tableInstanceJson, _ := json.MarshalIndent(tableInstance, "", "  ")
 		tableInstanceJson = append(tableInstanceJson, ","...)
